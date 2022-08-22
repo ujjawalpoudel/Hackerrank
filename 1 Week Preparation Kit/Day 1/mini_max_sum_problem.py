bd7613228ba71dea652db75b78ae4140f0e9e14b
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2022-08-22 10:43:58 +0545

    code formatting using black package

--- 1 Week Preparation Kit/Day 1/mini_max_sum_problem.py
+++ 1 Week Preparation Kit/Day 1/mini_max_sum_problem.py
@@ -4,20 +4,22 @@ find the minimum and maximum values that can be calculated by summing exactly fo
 Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 """
 
+
 def mini_max_sum(arr):
     # * Total sum value of array
     total_val = sum(arr)
-    
+
     # * Get maximum value from array
     max_val = max(arr)
-    
+
     # * Get minimum value from array
     min_val = min(arr)
-    
-    # * Print minimum sum and maximum sum from array
-    print(total_val-max_val, total_val-min_val)
 
-if __name__ == '__main__':
+    # * Print minimum sum and maximum sum from array
+    print(total_val - max_val, total_val - min_val)
+
+
+if __name__ == "__main__":
 
     arr = list(map(int, input().rstrip().split()))
 
