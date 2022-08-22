22116e3914d2f3f6f312a172c2b262de7938b704
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2022-08-22 10:33:04 +0545

    Day 1 question solved

---
+++ 1 Week Preparation Kit/Day 1/day1_solution.py
@@ -0,0 +1,38 @@
+"""
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
+Print the decimal value of each fraction on a new line with  places after the decimal.
+
+Note: This challenge introduces precision problems. 
+The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+"""
+
+
+def plus_minus(arr):
+    # Total number in array
+    total_count = len(arr)
+
+    # Calculate negative number
+    negative_count = len(list(filter(lambda x: (x < 0), arr)))
+
+    # Calculate positive number
+    positive_count = len(list(filter(lambda x: (x > 0), arr)))
+
+    # Calculate zero number
+    zero_count = len(list(filter(lambda x: (x == 0), arr)))
+
+    # proportion of positive values
+    print(round(positive_count / total_count, 5))
+
+    # proportion of negative values
+    print(round(negative_count / total_count, 5))
+
+    # proportion of zeros
+    print(round(zero_count / total_count, 5))
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plus_minus(arr)
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Hackerrank
 This repo contain practice question from Hackerrank.
+
+
+# SetUp
+
+1. Check that the correct python version is showing up by opening python interpreter.
+   ```bash
+   python --version
+   ```
+   Try closing and reopening the terminal if it doesn't work the first time.
+2. Setup up virtual environment and activate it.
+   ```bash
+   python -m venv venv
+   source venv/bin/activate
+   ```
+3. Install pip-tools.
+   ```bash
+   pip install pip-tools
+   ```
+4. Install dependencies
+   ```bash
+    pip-sync requirements.txt
+   ```
+
+## To install new package
+
+We will be using pip-tools for dependency management. This will help us to know how a specific package was installed in our venv. Do not run `pip install <package_name>` or `pip install -r requirements.txt` to install the packages. Please follow the steps to install and sync packages.
+
+1. Add package name to `requirements.in`
+2. Run `pip-compile requirements.in`. This will create two new files `requirements.txt`
+3. Run `pip-sync requirements.txt` to install the updated packages in the requirements file.
+
