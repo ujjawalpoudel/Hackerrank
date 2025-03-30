b63be73065f5d503a7bb0285768cfd73d5df0b80
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2025-03-30 19:47:21 -0400

    code update using black command for formatting

--- DSA/Sorting Algorithm/insertion_sort.py
+++ DSA/Sorting Algorithm/insertion_sort.py
@@ -1,36 +1,37 @@
 """
-You are given an unsorted list of integers. Implement the Insertion Sort algorithm to sort the list in ascending order.
+You are given an unsorted list of integers. I
+mplement the Insertion Sort algorithm to sort the list in ascending order.
 
-Insertion Sort works by iterating through the list and picking each element one by one. It compares the current element with the previous elements and places it in the correct position in the sorted part of the list. This process continues until the entire list is sorted.
-
-Time Complexity:
-- Worst-case: O(n²) (when the list is in descending order)
-- Best-case: O(n) (when the list is already sorted)
-- Average-case: O(n²)
+Insertion Sort works by iterating through the list and picking each element one by one. 
+It compares the current element with the previous elements and places it in the correct position in the sorted part of the list. 
+This process continues until the entire list is sorted.
 
 Example:
 Input:
 arr = [12, 11, 13, 5, 6]
+
 Output:
 [5, 6, 11, 12, 13]
 """
 
 from typing import List
 
+
 def insertion_sort(nums: List[int]) -> List[int]:
     n = len(nums)
-    
+
     for i in range(1, n):
         curr = nums[i]  # The element to be placed in the correct position
         j = i - 1  # Start comparing from the previous index
-        
+
         while j >= 0 and nums[j] > curr:
             nums[j + 1] = nums[j]  # Shift element to the right
             j -= 1
-        
+
         nums[j + 1] = curr  # Place the current element at the correct position
-    
+
     return nums  # Return the sorted list
+
 
 # Test Cases
 
